2ac59f81f7b58c82f3d42c87f1783ebf32bf3c0c
Nathan Stephani <nathanstephani12@gmail.com>
2025-02-09 14:08:19 -0600

    Add timer input and roll fuction
    Add input function for delay between rolls and function to generate
    dice rolls

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
+import random
+
 def GetPlayerCount() -> int:
     num_players = -1
     while(num_players != 3 and num_players != 4):
-        num_players = int(input('Input number of players (3 or 4):\n'))
+        num_players = int(input('Input number of players (3 or 4): '))
     return num_players
 
 def GetPlayers(a_num_players):
@@ -11,9 +13,21 @@ def GetPlayers(a_num_players):
         players.append(player)
     return players
 
+def GetCountdownInterval() -> int:
+    interval = -1
+    while(interval <= 0):
+        interval = int(input('Enter countdown interval in seconds: '))
+    return interval
+
+def Roll() -> int:
+    return random.randint(1, 6) + random.randint(1, 6)
+    
+
 def main():
     num_players = GetPlayerCount()
     players = GetPlayers(num_players)
+    countdown_interval = GetCountdownInterval()
+    
     print('RealTimeCatan')
     
 
